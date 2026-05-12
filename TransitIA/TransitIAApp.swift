4a7f221a3befa75295b8fa6ecd6560a7ad439a36
Omar Diaz <Omar Diaz>
2026-05-11 23:47:27 -0600

    Initial Commit

---
+++ TransitIA/TransitIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitIAApp.swift
+//  TransitIA
+//
+//  Created by Omar Díaz on 11/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TransitIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TransitIATests/TransitIATests.swift
@@ -0,0 +1,19 @@
+//
+//  TransitIATests.swift
+//  TransitIATests
+//
+//  Created by Omar Díaz on 11/05/26.
+//
+
+import Testing
+@testable import TransitIA
+
+struct TransitIATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
